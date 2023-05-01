5a24715976186687d44c6e7698ffa6daa02412ba
Wambaugh <marwambaugh@gmail.com>
2023-05-01 13:14:14 -0700

    checkingModels2Product

--- models/index.js
+++ models/index.js
@@ -1,16 +1,12 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 Product.belongsTo(Category);
 
-// Categories have many Products
 Category.hasMany(Product);
 
-// Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
@@ -18,7 +14,6 @@ Product.belongsToMany(Tag, {
   },
 });
 
-// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
